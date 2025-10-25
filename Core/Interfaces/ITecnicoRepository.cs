12fe47c1033675e8ec59b2b4c319744f034b455b
César Almeida <cesaralmeida89@gmail.com>
2025-10-25 15:02:13 +0200

    add ITecnicoRepository interface

---
+++ Core/Interfaces/ITecnicoRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.Models;
+
+namespace Core.Interfaces
+{
+    public interface ITecnicoRepository
+    {
+        Task<int> create(Tecnico tecnico);
+        Task<bool> delete(int id);
+        Task<List<Tecnico>> getAll();
+        Task<Tecnico?> getById(int id);
+        Task<bool> update(Tecnico tecnico);
+    }
+}
