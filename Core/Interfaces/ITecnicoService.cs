da4a8c079f2396aafe353870e92089f52b79f4b2
César Almeida <cesaralmeida89@gmail.com>
2025-10-25 21:36:44 +0200

    add ITecnicoService and TecnicoService, implement create method in TecnicoService

---
+++ Core/Interfaces/ITecnicoService.cs
@@ -0,0 +1,14 @@
+﻿using Core.Models;
+
+namespace Core.Interfaces
+{
+    public interface ITecnicoService
+    {
+        Task<(bool success, string message, int id)> create(Tecnico tecnico);
+        Task<(bool success, string message)> delete(int id);
+        Task<List<Tecnico>> getAll();
+        Task<Tecnico?> getById(int id);
+        Task<(bool success, string message)> update(Tecnico tecnico);
+        Task<Tecnico?> getByGaveta(int gaveta);
+    }
+}
---
+++ Core/TecnicoService.cs
@@ -0,0 +1,91 @@
+﻿using Core.Interfaces;
+using Core.Models;
+
+namespace Core
+{
+    public class TecnicoService : ITecnicoService
+    {
+        private readonly ITecnicoRepository _tecnicoRepository;
+
+        public TecnicoService(ITecnicoRepository tecnicoRepository)
+        {
+            _tecnicoRepository = tecnicoRepository;
+        }
+
+        public async Task<(bool success, string message, int id)> create(Tecnico tecnico)
+        {
+            if (string.IsNullOrWhiteSpace(tecnico.Nombre))
+            {
+                return (false, "El nombre del técnico es obligatorio.", 0);
+            }
+            if (string.IsNullOrWhiteSpace(tecnico.Apellidos))
+            {
+                return (false, "Los apellidos del técnico son obligatorios.", 0);
+            }
+            if (tecnico.Gaveta != null && tecnico.Gaveta > 0)
+            {
+                try
+                {
+                    var tecnicoByGaveta = await _tecnicoRepository.getByGaveta(tecnico.Gaveta.Value);
+                    if (tecnicoByGaveta != null)
+                    {
+                        return (false, "La gaveta del técnico ya está asignada a otro técnico.", 0);
+                    }
+                }
+                catch (Exception)
+                {
+                    return (false, "Error al comprobar la gaveta.", 0);
+                }
+            }
+            if (!string.IsNullOrWhiteSpace(tecnico.NombrePC))
+            {
+                try
+                {
+                    var tecnicoByNombrePC = await _tecnicoRepository.getByNombrePC(tecnico.NombrePC);
+                    if (tecnicoByNombrePC != null)
+                    {
+                        return (false, "El PC ya está asignado a otro técnico.", 0);
+                    }
+                }
+                catch (Exception)
+                {
+                    return (false, "Error al comprobar el nombre del PC.", 0);
+                }
+            }
+            try
+            {
+                int id = await _tecnicoRepository.create(tecnico);
+                return (true, $"Técnico creado correctamente con ID {id}.", id);
+            }
+            catch (Exception)
+            {
+                return (false, "Error al crear el técnico", 0);
+            } 
+        }
+
+        public Task<(bool success, string message)> delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Tecnico>> getAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tecnico?> getByGaveta(int gaveta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tecnico?> getById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<(bool success, string message)> update(Tecnico tecnico)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
