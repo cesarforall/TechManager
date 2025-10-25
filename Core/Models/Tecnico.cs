ba7d62963890231b878482da58d880323cb2e3b1
César Almeida <cesaralmeida89@gmail.com>
2025-10-25 14:58:38 +0200

    add Tecnico class

---
+++ Core/Models/Tecnico.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Models
+{
+    public class Tecnico
+    {
+        // init permite hacer el set solo en la inicialización del objeto
+        public int Id { get; init; }
+        public required string Nombre { get; set; }
+        public required string Apellidos { get; set; }
+        public string? Gaveta { get; set; }
+        public string? NombrePC { get; set; }
+        public string? UsuarioPC { get; set; }
+    }
+}
