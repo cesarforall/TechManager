51a6d19506876ffdbc4013db5af92c073398b152
César Almeida <cesaralmeida89@gmail.com>
2025-10-26 21:05:00 +0100

    implement ViewModelBase class

---
+++ UI/MVVM/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace UI.MVVM
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // Este método se llama para notificar a la vista que una propiedad ha cambiado
+        // El atributo CallerMemberName hace que el compilador llene automáticamente el nombre de la propiedad que llamó al método
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
