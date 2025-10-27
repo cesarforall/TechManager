9cf2a5a988320b47a95c8a7e03c4fe4d21eff062
César Almeida <cesaralmeida89@gmail.com>
2025-10-27 08:57:55 +0100

    implement RelayCommand class

---
+++ UI/MVVM/RelayCommand.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Input;
+
+namespace UI.MVVM
+{
+    public class RelayCommand : ICommand
+    {
+        // Delegados para guardar la referencia a los métodos de ejecución y verificación
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        // Evento que se dispara cuando cambia el estado de si se puede ejecutar el comando
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object,bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return canExecute == null || canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
