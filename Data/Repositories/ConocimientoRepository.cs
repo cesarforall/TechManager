be933a918ba9f086d660ec94fdb4b2eb83cc1a4b
César Almeida <cesaralmeida89@gmail.com>
2025-11-14 22:11:38 +0100

    add GetById method and fix return in Create method in ConocimientoRepository

--- Data/Repositories/ConocimientoRepository.cs
+++ Data/Repositories/ConocimientoRepository.cs
@@ -56,7 +56,7 @@ namespace Data.Repositories
                 command.Parameters.AddWithValue("$tecnico_id", conocimiento.TecnicoId);
                 command.Parameters.AddWithValue("$dispositivo_id", conocimiento.DispositivoId);
 
-                var result = await command.ExecuteNonQueryAsync();
+                var result = await command.ExecuteScalarAsync();
                 int newId = Convert.ToInt32(result);
 
                 return newId > 0 ? newId : 0;
@@ -152,6 +152,72 @@ namespace Data.Repositories
             catch (Exception)
             {
                 throw new RepositoryException("Error al obtener la lista de conocimientos de la base de datos.");
+            }
+        }
+
+        public async Task<Conocimiento> GetById(int id)
+        {
+            try
+            {
+                using var connection = new SqliteConnection(_connectionString);
+                await connection.OpenAsync();
+
+                using var command = connection.CreateCommand();
+                command.CommandText = """
+                    SELECT
+                        c.id AS conocimiento_id,
+                        c.tecnico_id AS conocimiento_tecnico_id,
+                        c.dispositivo_id AS conocimiento_dispositivo_id,
+                        t.id AS tecnico_id,
+                        t.nombre,
+                        t.apellidos,
+                        t.gaveta,
+                        t.nombre_pc,
+                        t.usuario_pc,
+                        d.id AS dispositivo_id,
+                        d.fabricante,
+                        d.modelo
+                    FROM conocimientos c
+                    LEFT JOIN tecnicos t ON c.tecnico_id = t.id
+                    LEFT JOIN dispositivos d ON c.dispositivo_id = d.id
+                    WHERE c.id = $id;
+                    """;
+                command.Parameters.AddWithValue("$id", id);
+
+                using var reader = await command.ExecuteReaderAsync();
+
+                if (await reader.ReadAsync())
+                {
+                    return new Conocimiento
+                    {
+                        Id = reader.GetInt32(0),
+                        TecnicoId = reader.GetInt32(1),
+                        DispositivoId = reader.GetInt32(2),
+                        Tecnico = new Tecnico
+                        {
+                            Id = reader.GetInt32(3),
+                            Nombre = reader.GetString(4),
+                            Apellidos = reader.GetString(5),
+                            Gaveta = reader.IsDBNull(6) ? null : reader.GetInt32(6),
+                            NombrePC = reader.IsDBNull(7) ? null : reader.GetString(7),
+                            UsuarioPC = reader.IsDBNull(8) ? null : reader.GetString(8),
+                        },
+                        Dispositivo = new Dispositivo
+                        {
+                            Id = reader.GetInt32(9),
+                            Fabricante = reader.GetString(10),
+                            Modelo = reader.GetString(11)
+                        }
+                    };
+                }
+                else
+                {
+                    return new Conocimiento();
+                }
+            }
+            catch (Exception)
+            {
+                throw new RepositoryException("Error al obtener el conocimiento en la base de datos.");
             }
         }
     }
