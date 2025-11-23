7c6d9dc1bf77a265740ceb04a5b475e02b3a44fa
César Almeida <cesaralmeida89@gmail.com>
2025-11-23 16:12:18 +0100

    add Actualizacion and Verificacion models

---
+++ Core/Models/Actualizacion.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Models
+{
+     public class Actualizacion
+    {
+        public int Id { get; set; } = 0;
+        public int DispositivoId { get; set; } = 0;
+        public Dispositivo Dispositivo { get; set; } = new();
+        public string Version { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public string Fecha { get; set; } = string.Empty;
+        public int Pendientes { get; set; } = 0;
+    }
+}
---
+++ Core/Models/Verificacion.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Models
+{
+    public class Verificacion
+    {
+        public int Id { get; set; } = 0;
+        public int ActualizacionId { get; set; } = 0;
+        public int TecnicoId { get; set; } = 0;
+        public Tecnico Tecnico { get; set; } = new();
+        public int Confirmado { get; set; } = 0;
+        public string FechaConfirmacion { get; set; } = string.Empty;
+    }
+}
